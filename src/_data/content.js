05a1aace3ffdbef0a2731ddc6f7da94fcc41660c
Doug Hanke <doug@amplifiedbydesign.com>
2022-11-10 21:20:19 -0800

    Footer tweaking, also 'tagline' and 'tagine' are different classes

--- src/_data/content.js
+++ src/_data/content.js
@@ -1,5 +1,5 @@
 const header = '<h1>Twitter is going great!</h1><p>... and definitely does not develop features primarily to stroke Elon Musk\'s delicate ego';
-const footer = 'Documenting the busiest period of Twitter updates since 2007.';
+const footer = '<p class="tagline">Documenting the busiest period of Twitter updates since 2007.</p><p><a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a> This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</p>';
 const entries = [
   {
     id: 'verified1',
