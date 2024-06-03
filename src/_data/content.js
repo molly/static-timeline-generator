31bcbe19618adf0106a2898440bfa90e94d4c3af
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2024-06-03 12:48:38 +0100

    add a couple of gigs

--- src/_data/content.js
+++ src/_data/content.js
@@ -2,44 +2,44 @@ const header = 'Welcome to this wonderful example timeline.';
 const footer = 'A footer goes here.';
 const entries = [
   {
-    id: 'cat',
-    categories: ['cat'],
+    id: 'hives-at-lcr',
     color: 'green',
-    faicon: 'cat',
-    datetime: '2021-01-01 05:00',
-    title: 'Cats are very good',
+    faicon: 'icons',
+    datetime: '2024-04-10 19:30',
+    title: 'The Hives',
     image: {
-      link: 'http://placekitten.com',
-      src: 'http://placekitten.com/200/300',
-      alt: 'A placeholder kitten',
-      caption: 'Kitten!',
+      src: 'img/gigs/the-hives-at-lcr.jpg',
+      alt: 'The Death of Randy Fitzsimmons Tour 2024',
+      caption: 'The Death of Randy Fitzsimmons Tour 2024',
     },
-    body: "The best thing in the universe is a cardboard box. Chirp at birds catasstrophe for meowwww for hiding behind the couch until lured out by a feathery toy for morning beauty routine of licking self sugar, my siamese, stalks me (in a good way), day and night lick the other cats. Pushed the mug off the table shove bum in owner's face like camera lens or tickle my belly at your own peril i will pester for food when you're in the kitchen even if it's salad and grass smells good, licks your face, but hiiiiiiiiii feed me now.",
+    body: "The Hives… on a creative hiatus… surely not? Yet, while it has been over ten years since the Swedish band’s last album, 2012’s Lex Hives, anyone fearing they have fallen into the ageing rock cliche of an act touring on past glories can rest easy. Yes, The Death Of Randy Fitzsimmons is The Hives first full album in over a decade, but as its macabre title hints, the reason for the band’s absence from the studio has been uniquely and suitably The Hives. There was a problem… the increasingly long absence of The Hives’ founder, mentor and songwriter, the limelight-shunning Randy Fitzsimmons. Now, returning for their final act since his mysterious disappearance, the band have delivered a record that is not just worthy of their legacy, but one that adds fresh lustre to The Hives. The Death Of Randy Fitzsimmons is a true renaissance.",
     links: [
       {
-        href: 'https://en.wikipedia.org/wiki/Cat',
-        linkText: 'Cat',
+        href: 'https://www.thehives.com/',
+        linkText: 'The Hives',
+      },
+      {
+        href: 'https://badnerves.co.uk/',
+        linkText: 'Bad Nerves (support)',
       },
     ],
   },
   {
-    id: 'dog',
-    categories: ['dog'],
+    id: 'livewire-at-waterfront',
     color: 'green',
-    faicon: 'dog',
-    datetime: '2021-05-01 23:55',
-    title: 'Puppies are great too',
+    faicon: 'icons',
+    datetime: '2024-04-20 18:30',
+    title: 'Livewire',
     image: {
-      link: 'http://place-puppy.com',
-      src: 'https://place-puppy.com/300x300',
-      alt: 'A placeholder puppy',
-      caption: 'Puppy!',
+      src: 'img/gigs/livewire-at-waterfront.jpg',
+      alt: 'Livewire the AC/DC Show',
+      caption: 'Livewire the AC/DC Show',
     },
-    body: 'Doggo ipsum sub woofer smol wow very biscit aqua doggo pupper dat tungg tho big ol pupper, very jealous pupper mlem heckin angery woofer very jealous pupper. Blep waggy wags long doggo, corgo. Tungg wow such tempt mlem very jealous pupper boofers lotsa pats, snoot smol big ol ruff doge super chub, long doggo heckin good boys and girls h*ck heck. Shoober blop many pats borkf, such treat. Big ol what a nice floof long water shoob wrinkler heck sub woofer, pupper porgo wow very biscit.',
+    body: 'AC/DC, arguably the greatest rock band of all time, are brought to life by Live/Wire with the true passion you would expect from professional musicians who share your love of this legendary live act. They deliver a critically acclaimed High Voltage two hour rock ’n’ roll set with not only cannons and a full bank of Marshall amps but a journey that takes fans from one of the best-selling albums of all time ‘Back in Black’ through to the 2020 release ‘PWR UP’, with a healthy dose of the classics from the Bon Scott era.<br><br>The Live/Wire show combines the vocal power of Podge Blacksmith as Brian Johnson, the virtuoso talent of Simon Davies as Angus Young and rhythmic accuracy of Dan Mosley as Malcolm Young. The line-up is completed by Eddie Clark as bassist Cliff Williams and Paul Alwin combining the groove of Phil Rudd and thunderous energy of Chris Slade on drums.<br><br>With over 20 years of touring under their belt, Live/Wire put the audience right in the middle of a live experience comparable to that only AC/DC themselves produce. We invite AC/DC fans to join us, sing with us and relive some of those classic AC/DC concerts of the past on our very own Highway to Hell.',
     links: [
       {
-        href: 'https://en.wikipedia.org/wiki/Dog',
-        linkText: 'Dog',
+        href: 'https://www.livewireacdcshow.com/',
+        linkText: 'Livewire',
       },
     ],
   },
@@ -48,7 +48,7 @@ const entries = [
 // Page details
 const pageTitle = 'Static timeline generator'; // The title of the page that shows in the browser tab
 const pageDescription = 'A super fancy timeline'; // The description of the page for search engines
-const pageAuthor = 'Jane Doe'; // Your name
+const pageAuthor = 'Richard Sistern'; // Your name
 
 // DON'T EDIT BELOW THIS LINE! --------------------------------------------------------------------
 const getFilters = (entries) => {
